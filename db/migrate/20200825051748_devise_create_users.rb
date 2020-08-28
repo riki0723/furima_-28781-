e2f787afe3386c284362113d93467d79402bfd30
riki0723 <rikishibata0723@gmail.com>
2020-08-28 14:14:07 +0900

    usersテーブルの作成

--- db/migrate/20200825051748_devise_create_users.rb
+++ db/migrate/20200825051748_devise_create_users.rb
@@ -4,8 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :familyname,         null: false
+      t.string :firstname,          null: false
+      t.string :familykana,         null: false
+      t.string :firstkana,          null: false
+      t.date   :berthday,           null: false
+      
+
+
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,14 @@
 ActiveRecord::Schema.define(version: 2020_08_25_051748) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "familyname", null: false
+    t.string "firstname", null: false
+    t.string "familykana", null: false
+    t.string "firstkana", null: false
+    t.date "berthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
