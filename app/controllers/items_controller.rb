f4aeac9b48ec04cd6ac2a06c328103d84439cf52
riki0723 <rikishibata0723@gmail.com>
2020-09-03 01:10:44 +0900

    createアクションのコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,4 +13,10 @@ class ItemsController < ApplicationController
   def create
     Item.create(item_params)
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :explation, :category_id, :status_id, :delivery_id, :area_id, :days_id, :price_id)
+  end
+
 end
