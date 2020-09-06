342ef42051721a621600753229bcdfb4f62ce98c
riki0723 <rikishibata0723@gmail.com>
2020-09-06 23:38:27 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to:'items#index'
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
+
+  resources :orders, only: :index
+
   
 end
