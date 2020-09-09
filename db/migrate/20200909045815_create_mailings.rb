5e01ecca645a8d1081d5f08a075d61e4327c6a29
riki0723 <rikishibata0723@gmail.com>
2020-09-09 14:48:25 +0900

    mailingのモデルデーターベース作成

---
+++ db/migrate/20200909045815_create_mailings.rb
@@ -0,0 +1,15 @@
+class CreateMailings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :mailings do |t|
+
+      t.string :adressnumber
+      t.integer :todouhuken_id
+      t.string :sichouson
+      t.string :banchi
+      t.string :bilding
+      
+
+      t.timestamps
+    end
+  end
+end
