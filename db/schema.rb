51e61c44eb5f6e812c578753e232746efdf650df
riki0723 <rikishibata0723@gmail.com>
2020-09-09 15:33:20 +0900

    バリデーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_020213) do
+ActiveRecord::Schema.define(version: 2020_09_09_045815) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,21 @@ ActiveRecord::Schema.define(version: 2020_09_04_020213) do
     t.integer "days_id"
     t.integer "price_id"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "mailings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "adressnumber"
+    t.integer "todouhuken_id"
+    t.string "sichouson"
+    t.string "banchi"
+    t.string "bilding"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
